10e362f48410cb2c7949bf4d0f13eccaccce9d4e
berwin <berwin7996@gmail.com>
2016-03-04 00:47:38 -0600

    routing and views set up

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,20 @@
 var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
-
+  $routeProvider
+    .when('/gallery', {
+      templateUrl : 'partials/gallery.html',
+      controller  : 'galleryController'
+    })
+    .when('/details/:rank', {
+      templateUrl : 'partials/details.html',
+      controller  : 'galleryController'
+    })
+    .when('/list', {
+      templateUrl : 'partials/list.html',
+      controller  : 'demoTwoController'
+    })
+    .otherwise({
+      redirectTo: '/gallery'
+    });
 })
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,48 @@
 /* Sample Controller */
+
+app.controller('galleryController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+
+
+  // $scope.classdata = {
+  //   "title": "cs498rk",
+  //   "names": ["abhi", "annie", "biplab", "devin", "jay", "sujay"]
+  // };
+
+  // $scope.change = function () {
+  //   $scope.classdata.names.push($scope.inputVal);
+  //   $scope.inputVal = "";
+  // };
+
+  // use this for getting image paths dynamically
+  $scope.path = 'data/images/';
+  $scope.rank = $routeParams.rank;
+
+  // changing between movies from details view
+  $scope.prev = function () {
+    // $scope.rank = $scope.rank - 1;
+    // if ($scope.rank < 0) { $scope.rank = 0; }
+    return (parseInt($scope.rank) - 1 >= 0) ? parseInt($scope.rank) - 1 : 0;
+  }
+  $scope.next = function () {
+    // $scope.rank = $scope.rank + 1;
+    // if ($scope.rank >= $scope.movies.length) { $scope.rank = $scope.movies.length - 1; }
+    return (parseInt($scope.rank) + 1 < 250) ? parseInt($scope.rank) + 1 : 249;
+  }
+
+  $http.get('data/imdb250.json').success(function (data) {
+    $scope.movies = data;
+    $scope.movie = $scope.movies[$scope.rank];
+  }).error(function (err){
+    console.log(err);
+  });
+
+}]);
+
+app.controller('detailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $scope.myRank = $routeParams.rank;
+
+}]);
+
+app.controller('listController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $scope.myRank = $routeParams.rank;
+}]);
